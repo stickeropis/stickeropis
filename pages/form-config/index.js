0cc64da914d7c5dd5bc4b5f9ad95e749ba2180ae
Sergey Kluchkovsky <kaineer@gmail.com>
2019-03-02 17:42:01 +0500

    Переименовывает cost в storyPOints, делает текстовым полем

--- pages/form-config/index.js
+++ pages/form-config/index.js
@@ -49,10 +49,6 @@ const priorityIds = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ];
 
-const costIds = [
-    1, 2, 3, 4, 5
-];
-
 const formInputs = [
     {
         id: 'id',
@@ -80,10 +76,9 @@ const formInputs = [
         choices: priorityIds
     },
     {
-        id: 'cost',
-        type: 'select',
-        label: 'Сложность',
-        choices: costIds
+        id: 'storyPoints',
+        type: 'text',
+        label: 'Сложность'
     },
     {
         id: 'date',
