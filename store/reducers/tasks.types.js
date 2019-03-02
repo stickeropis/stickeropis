7a7a281b731f8f5f59b935b691cb5593a092873b
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2019-03-02 13:57:14 +0500

    Моковые задачи

--- store/reducers/tasks.types.js
+++ store/reducers/tasks.types.js
@@ -19,3 +19,60 @@
 /**
  * @typedef {TaskM[]} Tasks
  */
+
+export default [
+    {
+        priority: 0,
+        storyPoints: 1,
+        author: '@ivanov',
+        title: 'Сверстать шапку',
+        description: 'Вывести название полей и контролы сортировки',
+        id: 'STICKER-101',
+        date: new Date(2019, 1, 2),
+        deadline: new Date(2019, 1, 2),
+        sprint: '#29. Мартовский код',
+        type: 'task',
+        tags: ['Вёрстка']
+    },
+    {
+        priority: 0,
+        storyPoints: 2,
+        author: '@ivanov',
+        title: 'Сверстать карточку',
+        description: 'Вывести все поля задачи',
+        id: 'STICKER-102',
+        date: new Date(2019, 1, 2),
+        deadline: new Date(2019, 1, 2),
+        sprint: '#29. Мартовский код',
+        type: 'task',
+        tags: ['Вёрстка']
+    },
+    {
+        priority: 0,
+        storyPoints: 3,
+        author: '@ivanov',
+        title: 'Починить прогрузку стилей',
+        description: 'При первом посещении сайта не применяются все стили',
+        id: 'STICKER-103',
+        date: new Date(2019, 1, 2),
+        deadline: new Date(2019, 1, 2),
+        sprint: '#29. Мартовский код',
+        type: 'bug'
+        tags: ['Вёрстка']
+    },
+    {
+        priority: 10,
+        storyPoints: 1,
+        author: '@ivanov',
+        title: ' Настроить деплой',
+        description: 'Задеплоить проект на heroku',
+        id: 'STICKER-104',
+        date: new Date(2019, 1, 2),
+        deadline: new Date(2019, 1, 2),
+        sprint: '#29. Мартовский код',
+        type: 'task',
+        link: ['STICKER-101', 'STICKER-102', 'STICKER-103'],
+        other: 'Собрать команду DevOps',
+        tags: ['Деплой']
+    }
+]
